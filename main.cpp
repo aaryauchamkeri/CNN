3632ba6bed3f904a6fc470ac76489313b0776398
Aarya Chamkeri <aaryachamkeri06@gmail.com>
2025-07-28 15:49:22 -0700

    Added testing data and got 77.56% accuracy.

--- main.cpp
+++ main.cpp
@@ -31,9 +31,9 @@ int main() {
     Tensor4D train_images;
     vector<int> train_labels;
 
-    MnistLoader::load("../data/train-images-idx3-ubyte", "../data/train-labels-idx1-ubyte", train_images, train_labels, 1000);
+    MnistLoader::load("../data/train-images-idx3-ubyte", "../data/train-labels-idx1-ubyte", train_images, train_labels);
 
-    int epochs = 30;
+    int epochs = 10;
     float lr = 0.05;
 
     Convolution conv({1, 28, 28}, 3, 1, 16, lr);
@@ -76,6 +76,40 @@ int main() {
         cout << "Epoch " << (epoch + 1) << ": Loss = " << avg_loss
              << ", Accuracy = " << accuracy << "%" << endl;
     }
+
+    Tensor4D test_images;
+    vector<int> test_labels;
+
+    MnistLoader::load("../data/t10k-images-idx3-ubyte", "../data/t10k-labels-idx1-ubyte", test_images, test_labels);
+    float total_loss = 0.0f;
+    int correct = 0;
+    for (int i = 0; i < test_images.size(); ++i) {
+        Tensor4D x = {test_images[i]};
+        Tensor4D conv_out = conv.forward(x);
+        Tensor4D relu_out = relu.forward(conv_out);
+        Tensor4D pool_out = pool.forward(relu_out);
+        vector<float> flat = flatten.forward(pool_out);
+        vector<float> logits = dense.forward(flat);
+        vector<float> probs = softmax.forward(logits);
+
+        int label = test_labels[i];
+        float loss = -log(probs[label] + 1e-8f);
+        total_loss += loss;
+
+        int pred = max_element(probs.begin(), probs.end()) - probs.begin();
+        if (pred == label) correct++;
+        vector<float> d_softmax = softmax.backward(label);
+        vector<float> d_dense = dense.backward(d_softmax);
+        Tensor4D d_unflat = unflatten(d_dense);           
+        Tensor4D d_pool = pool.backward(d_unflat);
+        Tensor4D d_relu = relu.backward(d_pool);
+        conv.backward(d_relu);
+    }
+
+    float avg_loss = total_loss / test_images.size();
+    float accuracy = 100.0f * correct / test_images.size();
+    cout << "Test: " << "Loss = " << avg_loss
+            << ", Accuracy = " << accuracy << "%" << endl;
 
     return 0;
 }
